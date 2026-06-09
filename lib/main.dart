6b8bfc3ce23aea7d7455a44fd72b2f0edd920acc
Yeo CTO (Paperclip) <sndlabs25@gmail.com>
2026-06-10 01:25:02 +0900

    fix: change API URL from localhost to LAN IP (172.20.0.108:8000)

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,9 @@ class CafeVisionService extends ChangeNotifier {
 
   bool get loading => _loading;
 
+  // Mac IP on local network (update if your IP changes)
+      static const String serverUrl = 'http://172.20.0.108:8000';
+
   Future<void> analyzeImage(XFile image) async {
     _loading = true;
     notifyListeners();
@@ -49,7 +52,7 @@ class CafeVisionService extends ChangeNotifier {
     try {
       final request = http.MultipartRequest(
         'POST',
-        Uri.parse('http://127.0.0.1:8000/api/analyze'),
+        Uri.parse('$serverUrl/api/analyze'),
       );
       request.files.add(await http.MultipartFile.fromPath('file', image.path));
       final response = await request.send();
